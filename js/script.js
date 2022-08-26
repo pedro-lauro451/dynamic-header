9e3fdd82a8fb35c2bafc039dacea4a6b2fb168bd
Pedro <jinnpheonix@protonmail.com>
2022-08-25 23:07:12 -0300

    lang option added

--- js/script.js
+++ js/script.js
@@ -1,8 +1,58 @@
-var divButton = document.getElementById('divButton');
+    var divButton = document.getElementById('divButton');
+    var langButton = document.getElementById('langButton');
     var corpo = document.body;
     var darkMode = false;
+    var isEn = true;
+    
     const togglecontent = document.querySelectorAll('.content');
     const toggleHeadercontent = document.querySelectorAll('.header');
+
+    const toggleLangEn = document.querySelectorAll('.english');
+    const toggleLangPt = document.querySelectorAll('.portuguese');
+
+    divButton.innerText = "Dark Mode";
+
+    langButton.onclick = function()
+    {
+        if(isEn === true)
+        {
+            toggleLangPt.forEach(content => {
+                content.style.display = "initial";
+            });
+    
+            toggleLangEn.forEach(content => {
+                content.style.display = "none";
+            });
+            isEn = false;
+            if(darkMode === false)
+            {
+                divButton.innerText = "Modo Escuro";
+            }
+            if(darkMode === true)
+            {
+                divButton.innerText = "Modo Claro";
+            }
+        }
+        else if(isEn === false)
+        {
+            toggleLangPt.forEach(content => {
+                content.style.display = "none";
+            });
+    
+            toggleLangEn.forEach(content => {
+                content.style.display = "initial";
+            });
+            isEn = true;
+            if(darkMode === false)
+            {
+                divButton.innerText = "Dark Mode";
+            }
+            if(darkMode === true)
+            {
+                divButton.innerText = "Light Mode";
+            }
+        }
+    }
 
     divButton.onclick = function()
     {
@@ -20,7 +70,14 @@ var divButton = document.getElementById('divButton');
             });
 
             darkMode = true;
-            divButton.innerText = 'Light Mode';
+            if(isEn === true)
+            {
+                divButton.innerText = "Light Mode";
+            }
+            if(isEn === false)
+            {
+                divButton.innerText = "Modo Claro";
+            }
         }
 
         else if(darkMode == true)
@@ -37,6 +94,13 @@ var divButton = document.getElementById('divButton');
             });
 
             darkMode = false;
-            divButton.innerText = 'Dark Mode';
+            if(isEn === true)
+            {
+                divButton.innerText = "Dark Mode";
+            }
+            if(isEn === false)
+            {
+                divButton.innerText = "Modo Escuro";
+            }
         }
     }
